eb73c93088492146e58b2d1cbdad666c908166f4
Mike McNeeley <mmcneeley@gmail.com>
2018-02-26 03:20:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(response)
-  if response == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif response == response.uppercase
+  elsif phrase == phrase.uppercase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
